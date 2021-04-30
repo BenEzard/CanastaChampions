82fe6d505e603fe468f8a179bc8aea6025ccfd85
timb <bmadtiger@outlook.com>
2021-04-30 16:35:47 +0930

    Moved registering the event handler in the View to the new DataContextChanged event handler so that we have access to the DataContext / ViewModel.

--- MvxCanastaChampions.Wpf/Views/CompetitionView.xaml.cs
+++ MvxCanastaChampions.Wpf/Views/CompetitionView.xaml.cs
@@ -12,18 +12,31 @@ namespace MvxCanastaChampions.Wpf.Views
     public partial class CompetitionView : MvxWpfView
     {
         CompetitionViewModel _viewModel;
-        MediaPlayer _mediaPlayer;
+        //MediaPlayer _mediaPlayer;
 
         public CompetitionView()
         {
             InitializeComponent();
 
-            if (this.ViewModel is CompetitionViewModel viewModel)
+            // Since DataContext / ViewModel is not set at time of construction, setup an event handler to register the view model's event handler when the DataContext is set.
+            this.DataContextChanged += this.CompetitionView_DataContextChanged;
+
+            //_mediaPlayer = new MediaPlayer();
+        }
+
+        private void CompetitionView_DataContextChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+        {
+            // First Check the DataContext, then the ViewModel, but it appears they are both the same...
+            // see https://www.mvvmcross.com/documentation/fundamentals/data-binding#a-note-about-datacontext
+            if (this.DataContext is CompetitionViewModel viewModel)
             {
+                // I'm preferencing the DataContext since that is the property that triggered this event
                 _viewModel = viewModel;
             }
-
-            _mediaPlayer = new MediaPlayer();
+            else if (this.ViewModel is CompetitionViewModel competitionViewModel)
+            {
+                _viewModel = competitionViewModel;
+            }
 
             if (_viewModel != null)
             {
@@ -37,8 +50,8 @@ namespace MvxCanastaChampions.Wpf.Views
         /// </summary>
         private void ViewModel_CompetitionStarted(object sender, System.EventArgs e)
         {
-            _mediaPlayer.Open(new System.Uri("path_to_some_media_file"));
-            _mediaPlayer.Play();
+            //_mediaPlayer.Open(new System.Uri("path_to_some_media_file"));
+            //_mediaPlayer.Play();
 
             // Alternatively, play whatever sound is associated with the Asterisk sound event in Windows.
             System.Media.SystemSounds.Asterisk.Play();
